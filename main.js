c43d1626c8e764303b6fbc53f31ff94d1337cf7c
Ea-st-ring <95581482+Ea-st-ring@users.noreply.github.com>
2023-01-11 00:33:10 +0900

    [ALL] 사진 div 및 이벤트 추가

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const equip_list = document.querySelector('.equip_list');
 scroll_btn.addEventListener('click',()=>{
     equip_list.style.display = "flex";
     equip_list.classList.add('active');
-    scrollIntoView('.equip_list');
+    setTimeout(() => scrollIntoView('.equip_list'), 300);
 });
 
 function scrollIntoView(selector) {
@@ -18,3 +18,47 @@ window.addEventListener('load',()=>{
         planet.classList.add('active');
     }, 3000);
 });
+
+
+const imgClasses = [
+    '.celestron_imgs',
+    '.skywatcher_imgs',
+    '.vixen_imgs'
+];
+
+const btnIds = [
+    '#celestron__btn',
+    '#skywatcher__btn',
+    '#vixen__btn'
+];
+
+
+
+const telescopeImgs = imgClasses.map(cls => document.querySelector(cls));
+const telescopeBtns = btnIds.map(id =>document.querySelector(id));
+
+
+telescopeBtns.forEach((target)=>{
+    target.addEventListener('click',()=>{
+        showImgs(target.id);
+    });
+});
+
+function showImgs(btn){
+    if(btn == 'celestron__btn'){
+        telescopeImgs[0].classList.add('active');
+        telescopeImgs[1].classList.remove('active');
+        telescopeImgs[2].classList.remove('active');
+    }
+    else if(btn == 'skywatcher__btn'){
+        telescopeImgs[0].classList.remove('active');
+        telescopeImgs[1].classList.add('active');
+        telescopeImgs[2].classList.remove('active');
+    }
+    else if(btn=='vixen__btn'){
+        telescopeImgs[0].classList.remove('active');
+        telescopeImgs[1].classList.remove('active');
+        telescopeImgs[2].classList.add('active');
+    }
+    equip_list.classList.add('moveLeft');
+}
